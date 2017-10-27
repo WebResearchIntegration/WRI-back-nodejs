e44436aa7d6660674a348b7cb616490ad045f50a
Simbadev <maxime.gasnier.1@gmail.com>
2017-10-27 03:15:50 +0200

    add basic responses with 200

--- api/articles/article.controller.js
+++ api/articles/article.controller.js
@@ -1,28 +1,28 @@
 
 exports.show = function(req, res) {
-
+    res.send(200);
 }
 
 exports.create = function(req, res) {
-    
+    res.send(200);
 }
 
 exports.update = function(req, res) {
-    
+    res.send(200);
 }
 
 exports.delete = function(req, res) {
-    
+    res.send(200);
 }
 
 exports.createRelationship = function(req, res) {
-    
+    res.send(200);
 }
 
 exports.updateRelationship = function(req, res) {
-    
+    res.send(200);
 }
 
 exports.deleteRelationship = function(req, res) {
-    
+    res.send(200);
 }
