ae0c3e5aea6cb83c6d3883297903786b359243b5
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-05 03:27:29 +0100

    Add routes inside index.js

--- api/notes/index.js
+++ api/notes/index.js
@@ -1,2 +1,11 @@
 /** @module notes/index */
 
+const noteRouter     = require('express').Router(),
+      noteController = require('./note.controller');
+
+noteRouter.get('/:id', noteController.show);
+noteRouter.post('/', noteController.create);
+noteRouter.put('/:id', noteController.update);
+noteRouter.delete('/:id', noteController.delete);
+
+noteRouter.get('/', noteController.getAll);
