367fa999e40a3e94181522a64e1bc1072045f5a4
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-05 03:52:27 +0100

    basic cRUD methods

--- api/notes/note.controller.js
+++ api/notes/note.controller.js
@@ -1,3 +1,23 @@
 /** @module notes/note.controller */
 
 const Note = require('./note.model').Note;
+
+exports.show = function(req, res) {
+
+}
+
+exports.create = function(req, res) {
+
+}
+
+exports.update = function(req, res) {
+
+}
+
+exports.delete = function(req, res) {
+
+}
+
+exports.getAllNotes = function(req, res) {
+    
+} 
