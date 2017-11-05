8758c4f57bd2958d48353e1193e333d3cc31419d
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-05 14:35:25 +0100

    add create method inside controller methods

--- api/notes/note.controller.js
+++ api/notes/note.controller.js
@@ -7,7 +7,17 @@ exports.show = function(req, res) {
 }
 
 exports.create = function(req, res) {
-
+    let node = new Note({text: req.body.text});
+    
+    node.save((err, nodeFromDatabase) => {
+        if(err) {
+            console.log('ERR => ', err);
+            res.sendStatus(500);
+        } else {
+            res.send({success: true, node: nodeFromDatabase});
+            console.log('Success : ' + nodeFromDatabase)
+        }
+    });
 }
 
 exports.update = function(req, res) {
--- api/notes/note.model.js
+++ api/notes/note.model.js
@@ -4,12 +4,28 @@ const db = require('seraph')({
     pass: "root",
     id: "id"
 }),
+    moment = require('moment'),
     nodeModelGenerator = require('seraph-model'),
     NoteNode = nodeModelGenerator(db, 'Note');
 
-/** */
+/** 
+* Class representing a note and that can interacts directly with neo4j cypher requests.
+*/
 class Note {
     constructor(objectNote) {
+        this.createdAt = moment().format('MMMM Do YYYY, h:mm:ss a');
+        this.text = objectNote.text;
+        this.nodeNeo4j = null;
+    }
 
+    save(callback) {
+        NoteNode.save({
+            text: this.text,
+            createdAt: this.createdAt
+        }, (err, noteNode) => {
+            if(err) callback(err, null);
+            if(callback && !err) callback(null, callback);
+            this.nodeNeo4j = noteNode;
+        });
     }
 }
