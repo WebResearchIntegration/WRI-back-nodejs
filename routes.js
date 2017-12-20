a2e5c4d61ffe7de617b4198df77ef09bb57111d5
Simbadev <maxime.gasnier.1@gmail.com>
2017-12-20 14:33:56 +0100

    add authors to route

--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
     app.use('/api/article/', require('./api/articles'));
     app.use('/api/note/', require('./api/notes'));
     app.use('/api/question/', require('./api/questions'));
+    app.use('/api/author/', require('./api/authors'));
 };
